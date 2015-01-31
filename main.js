930b01a9932797371a5715244f530cc94e6530ba
CamiloMM <herzschaltung@yahoo.com.ar>
2015-01-31 12:27:32 -0200

    Load the image given as an argument, and center-align down-fit it with pure CSS.

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-document.write(require('nw.gui').App.argv)
+var fs      = require('fs');
+var gui     = require('nw.gui');
+var $       = require('jquery');
+var fileUrl = require('file-url');
+
+$(function() {
+    var file = gui.App.argv[0];
+    fs.exists(file, function (exists) {
+        if (exists) {
+            var url = fileUrl(file);
+            $('#viewer').append('<img src="' + url + '" />');
+        } else {
+            $('#viewer').append('<p>File does not exist</p>');
+        }
+    });
+});
