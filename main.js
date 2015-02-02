a5850aa4599b1229e70147262fa8e42f1657ae3b
CamiloMM <herzschaltung@yahoo.com.ar>
2015-02-02 13:59:46 -0200

    Write the viewer mode on DOM.

--- main.js
+++ main.js
@@ -3,12 +3,20 @@ var gui     = require('nw.gui');
 var $       = require('jquery');
 var fileUrl = require('file-url');
 
+// This is kind of a visual convenience, just consider other
+// files should interpret this one as "the app's namespace".
+var dojikko = exports;
+
+// Modes are 'fitdown', 'fit' and 'zoom'.
+dojikko.viewerMode = 'fitdown';
+
 function showNotFound() {
     $('#viewer').append('<p>File does not exist</p>');
 }
 
 function showFile(file) {
     var url = fileUrl(file);
+    $('#viewer').data('mode', dojikko.viewerMode);
     $('#viewer').append('<img src="' + url + '" />');
 }
 
