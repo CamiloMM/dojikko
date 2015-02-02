16a80098bf19ba8ebfb9aad9eb05a8b76ef95c4b
CamiloMM <herzschaltung@yahoo.com.ar>
2015-02-01 23:43:37 -0200

    A bit of abstraction.

--- main.js
+++ main.js
@@ -3,14 +3,22 @@ var gui     = require('nw.gui');
 var $       = require('jquery');
 var fileUrl = require('file-url');
 
+function showNotFound() {
+    $('#viewer').append('<p>File does not exist</p>');
+}
+
+function showFile(file) {
+    var url = fileUrl(file);
+    $('#viewer').append('<img src="' + url + '" />');
+}
+
 $(function() {
     var file = gui.App.argv[0];
     fs.exists(file, function (exists) {
         if (exists) {
-            var url = fileUrl(file);
-            $('#viewer').append('<img src="' + url + '" />');
+            showFile(file);
         } else {
-            $('#viewer').append('<p>File does not exist</p>');
+            showNotFound();
         }
     });
 });
