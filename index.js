640878385b45fa6296fcfe9c732121e09d8ebd30
CamiloMM <herzschaltung@yahoo.com.ar>
2015-02-03 07:08:05 -0200

    Fixed several issues and made it all work again.

---
+++ index.js
@@ -0,0 +1,9 @@
+
+// Note that NW.js has a webkit context and a node context.
+// This provides a central export for all the app-wide logic.
+
+// The app's "namespace"
+var dojikko = module.exports;
+
+// Modes are 'fitdown', 'fit' and 'zoom'.
+dojikko.viewerMode = 'fitdown';
--- main.js
+++ main.js
@@ -3,12 +3,7 @@ var gui     = require('nw.gui');
 var $       = require('jquery');
 var fileUrl = require('file-url');
 
-// This is kind of a visual convenience, just consider other
-// files should interpret this one as "the app's namespace".
-var dojikko = exports;
-
-// Modes are 'fitdown', 'fit' and 'zoom'.
-dojikko.viewerMode = 'fitdown';
+var dojikko = process.mainModule.exports;
 
 function showNotFound() {
     $('#viewer').append('<p>File does not exist</p>');
@@ -16,7 +11,7 @@ function showNotFound() {
 
 function showFile(file) {
     var url = fileUrl(file);
-    $('#viewer').data('mode', dojikko.viewerMode);
+    $('#viewer').attr('data-mode', dojikko.viewerMode);
     $('#viewer').append('<img src="' + url + '" />');
 }
 
